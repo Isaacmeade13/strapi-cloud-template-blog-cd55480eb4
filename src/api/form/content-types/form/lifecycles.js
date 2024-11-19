e38f34fc3ae1adfc3a843ef8cb7c8f42d6760715
Jorj Hambaryan <jorjhambaryan@Jorjs-MacBook-Pro.local>
2024-11-19 11:25:53 +0400

    feat/form

---
+++ src/api/form/content-types/form/lifecycles.js
@@ -0,0 +1,21 @@
+module.exports = {
+  async afterCreate(event) {
+    const { result } = event;
+
+    const documentId = result?.documentId;
+    const adminPath = `/admin/content-manager/collection-types/api::filter.filter/${documentId}`;
+    const baseURL = process.env.URL || "http://localhost:1337";
+    const fullURL = `${baseURL}${adminPath}`;
+    const emailOptions = {
+      to: "Info@eventcage.com",
+      subject: `New entity in Form table `,
+      text: `New entity in ->  ${fullURL}`,
+    };
+    try {
+      await strapi.plugin("email").service("email").send(emailOptions);
+      strapi.log.info(`Retrieve email sent to '' for location ID `);
+    } catch (error) {
+      strapi.log.error(`Failed to send retrieve email: ${error.message}`);
+    }
+  },
+};
